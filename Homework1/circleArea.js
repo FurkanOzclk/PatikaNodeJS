3104d199de31f0d670f03930a5a34266adcbf951
Furkan Özçelik <93225049+FurkanOzclk@users.noreply.github.com>
2024-03-22 23:10:42 +0300

    Delete Homework1 directory

--- Homework1/circleArea.js
+++
@@ -1,8 +0,0 @@
-const pi = 3.14;
-const args = process.argv.slice(2);
-
-function circleArea(radius) {
-    let area = pi * radius * radius;
-    console.log(`Yarıçapı ${args} olan darirenin alanı: ${area}`);
-}
-circleArea(args * 1);
